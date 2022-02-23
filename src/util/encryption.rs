e9e3f69e64d3f78d5c594218de5b258eb5ddd56c
ewoutvdb <vdbewout@gmail.com>
2022-02-23 11:48:44 +0100

    improved hash

--- src/util/encryption.rs
+++ src/util/encryption.rs
@@ -1,13 +1,30 @@
-use argon2::{self, Config};
+use rand::RngCore;
 
 use crate::error::Result;
+
+const SALT_LEN: usize = 32;
+
+fn argon2_config<'a>() -> argon2::Config<'a> {
+    return argon2::Config {
+        variant: argon2::Variant::Argon2id,
+        hash_length: 32,
+        lanes: 4,
+        mem_cost: 16 * 1024,
+        time_cost: 32,
+        ..Default::default()
+    };
+}
 
 pub(crate) async fn hash_password(password: String) -> Result<String> {
     let (send, recv) = tokio::sync::oneshot::channel();
 
     rayon::spawn(move || {
-        let salt: String = (0..32).map(|_| rand::random::<char>()).collect();
-        let result = argon2::hash_encoded(password.as_bytes(), salt.as_bytes(), &Config::default());
+        let mut salt = [0u8; SALT_LEN];
+        rand::rngs::OsRng.fill_bytes(&mut salt);
+
+        let argon2_config = argon2_config();
+
+        let result = argon2::hash_encoded(password.as_bytes(), &salt, &argon2_config);
         let _ = send.send(result);
     });
 
