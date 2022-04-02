0c897d7e5269bf81db5d3796315c634686c6fc2f
ewoutvdb <vdbewout@gmail.com>
2022-04-02 15:14:44 +0200

    Remove permissive Cors on release

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,10 @@ extern crate diesel;
 
 use axum::{extract::Extension, Router};
 use tower::ServiceBuilder;
-use tower_http::{cors::CorsLayer, trace::TraceLayer};
+use tower_http::trace::TraceLayer;
+
+#[cfg(debug_assertions)]
+use tower_http::cors::CorsLayer;
 
 use config::db::postgres::PgPool;
 
@@ -25,14 +28,15 @@ mod util;
 pub fn app(pg_pool: PgPool) -> Router {
     let middleware_stack = ServiceBuilder::new()
         .layer(TraceLayer::new_for_http())
-        .layer(CorsLayer::permissive())
-        .layer(Extension(pg_pool))
-        .into_inner();
+        .layer(Extension(pg_pool));
+
+    #[cfg(debug_assertions)]
+    let middleware_stack = middleware_stack.layer(CorsLayer::permissive());
 
     Router::new()
         .nest("/auth", handler::auth::routes())
         .nest("/state", handler::state::routes())
         .nest("/task", handler::task::routes())
         .nest("/health", handler::health::routes())
-        .layer(middleware_stack)
+        .layer(middleware_stack.into_inner())
 }
